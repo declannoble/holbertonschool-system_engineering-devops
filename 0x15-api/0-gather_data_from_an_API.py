37e15683731e53dd83f966849b30593d43c351cb
declannoble <declanjnoble@hotmail.com>
2022-09-12 18:52:28 +1000

    fix: pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""
-script to query an api and return information about employees todo list progress
-"""
 
-import requests
-from sys import argv
+from urllib import request
+
 
 if __name__ == "__main__":
-        num_tasks = 0
-        completed = 0
-        completed_tasks = []
-        r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                         .format(argv[1])).json()
-        r2 = requests.get(
+    num_tasks = 0
+    completed = 0
+    completed_tasks = []
+    uQuery = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                          .format(argv[1])).json()
+    tQuery = requests.get(
                 'https://jsonplaceholder.typicode.com/todos/?userId={}'
                 .format(argv[1])).json()
-        for task in r2:
-                num_tasks += 1
-                if task.get('completed') is True:
-                        completed += 1
-                        completed_tasks.append(task.get('title'))
+    for tasks in tQuery:
+        num_tasks += 1
+        num_tasks += 1
+        if tasks.get('completed') is True:
+            completed += 1
+            completed_tasks.append(tasks.get('title'))
         print("Employee {} is done with tasks({}/{}):".format(
-                r.get('name'), completed, num_tasks))
+                uQuery.get('name'), completed, num_tasks))
         for item in completed_tasks:
-                print("\t {}".format(item))
+            print("\t {}".format(item))
