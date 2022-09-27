67bf61a2ef7d3e73a5c5b6e40eaf1e7f62444ffe
declannoble <declanjnoble@hotmail.com>
2022-09-27 10:03:37 +1000

    fix: invalid subreddit requirement

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,11 +11,11 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'HolbertonSchoolApiRequests (by /u/declannoble)'}
     req = requests.get(url, headers=headers)
-    if req.status_code == 404:
-        return None
-    data = req.json().get('data')
-    children = data.get('children')
-
-    for posts in children:
-        postDict = posts.get('data')
-        print(postDict.get('title'))
+    if req.status_code != 404:
+        data = req.json().get('data')
+        children = data.get('children')
+        for posts in children:
+            postDict = posts.get('data')
+            print(postDict.get('title'))
+    else:
+        print(None)
